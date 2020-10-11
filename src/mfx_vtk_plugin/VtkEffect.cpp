f09e919a6f1403eba8d6c6f6b3f496fa4d6ffc0b
Tomas Karabela <tkarabela@seznam.cz>
2020-10-11 23:32:58 +0200

    Support 2-vertex faces (edges) for OFX interaction
    - input 2-vertex faces get converted into `vtkLine` cells
    - output `vtkLine`/`vtkPolyLine` cells get converted to 2-vertex faces

--- src/mfx_vtk_plugin/VtkEffect.cpp
+++ src/mfx_vtk_plugin/VtkEffect.cpp
@@ -27,6 +27,7 @@ THE SOFTWARE.
 #include <cassert>
 #include <chrono>
 #include <vtkXMLPolyDataWriter.h>
+#include <vtkCellArrayIterator.h>
 
 static void strided_copy(void *dest, void *src, int size, int count, int dest_stride, int src_stride) {
     assert(dest != nullptr);
@@ -58,6 +59,8 @@ OfxStatus VtkEffect::Describe(OfxMeshEffectHandle descriptor) {
 }
 
 OfxStatus VtkEffect::Cook(OfxMeshEffectHandle instance) {
+    printf("== VtkEffect::Cook\n");
+
     auto t_cook_start = std::chrono::system_clock::now();
     MfxMesh input = GetInput(kOfxMeshMainInput).GetMesh();
 
@@ -69,21 +72,27 @@ OfxStatus VtkEffect::Cook(OfxMeshEffectHandle instance) {
     input.GetVertexAttribute(kOfxMeshAttribVertexPoint).FetchProperties(vertPoint);
     input.GetFaceAttribute(kOfxMeshAttribFaceCounts).FetchProperties(faceLen);
 
+    printf("MFX input has %d points, %d vertices, %d faces\n",
+           inputProps.pointCount, inputProps.vertexCount, inputProps.faceCount);
+
+    auto t_cook_after_mfx_prologue = std::chrono::system_clock::now();
+
     // ------------------------------------------------------------------------
     // Create vtkPolyData from MFX mesh
-    // TODO optimize
     // ------------------------------------------------------------------------
     auto vtk_input_polydata = vtkSmartPointer<vtkPolyData>::New();
     auto vtk_input_points = vtkSmartPointer<vtkPoints>::New();
     auto vtk_input_polys = vtkSmartPointer<vtkCellArray>::New();
-    // ...and isolated vertices - not supported by MFX
-    // ...and face-less edges - not supported by MFX
+    auto vtk_input_lines = vtkSmartPointer<vtkCellArray>::New();
+    // handle isolated vertices - do we want to make vtkVertex:es out of them? (perhaps not)
 
     vtk_input_polydata->SetPoints(vtk_input_points);
+    vtk_input_polydata->SetLines(vtk_input_lines);
     vtk_input_polydata->SetPolys(vtk_input_polys);
 
     // make VTK use the same datatypes as MFX
     vtk_input_points->SetDataTypeToFloat();
+    vtk_input_lines->Use32BitStorage();
     vtk_input_polys->Use32BitStorage();
 
     // copy points
@@ -98,25 +107,35 @@ OfxStatus VtkEffect::Cook(OfxMeshEffectHandle instance) {
                  pointPos.stride);
 
     // copy cells
-    vtk_input_polys->AllocateExact(inputProps.faceCount, inputProps.vertexCount); // just a hint
+
+    // pre-allocation hints
+    if (inputProps.vertexCount >= 3*inputProps.faceCount) {
+        // looks like polygonal mesh, expect 100% polys
+        vtk_input_polys->AllocateExact(inputProps.faceCount, inputProps.vertexCount);
+    } else {
+        // at least some faces have < 3 vertices, expect 100% lines
+        vtk_input_lines->AllocateExact(inputProps.faceCount, inputProps.vertexCount);
+    }
 
     int vertex_idx = 0;
     for (int face_idx = 0; face_idx < inputProps.faceCount; face_idx++) {
         int vertcount = *((int*)(&faceLen.data[face_idx * faceLen.stride]));
-        bool skip = false;
+        vtkCellArray *cell_arr = nullptr;
 
-        if (vertcount < 3) {
-            skip = true; // XXX how to handle isolated vertices/edges?
+        if (vertcount == 2) {
+            cell_arr = vtk_input_lines;
+        } else if (vertcount >= 3) {
+            cell_arr = vtk_input_polys;
         }
 
-        if (!skip) {
-            vtk_input_polys->InsertNextCell(vertcount);
+        if (cell_arr) {
+            cell_arr->InsertNextCell(vertcount);
         }
 
         for (int i = 0; i < vertcount; i++, vertex_idx++) {
             int vertex_arr = *((int*)(&vertPoint.data[vertex_idx * vertPoint.stride]));
-            if (!skip){
-                vtk_input_polys->InsertCellPoint(vertex_arr);
+            if (cell_arr) {
+                cell_arr->InsertCellPoint(vertex_arr);
             }
         }
     }
@@ -136,9 +155,9 @@ OfxStatus VtkEffect::Cook(OfxMeshEffectHandle instance) {
 
     auto vtk_output_polydata = vtkSmartPointer<vtkPolyData>::New();
 
-    auto t_cook_inner_start = std::chrono::system_clock::now();
+    auto t_cook_after_vtk_prologue = std::chrono::system_clock::now();
     OfxStatus cook_status = vtkCook(vtk_input_polydata, vtk_output_polydata);
-    auto t_cook_inner_end = std::chrono::system_clock::now();
+    auto t_cook_after_vtk_cook = std::chrono::system_clock::now();
 
     if (cook_status != kOfxStatOK) {
         input.Release();
@@ -148,31 +167,64 @@ OfxStatus VtkEffect::Cook(OfxMeshEffectHandle instance) {
 //    {
 //        auto writer = vtkSmartPointer<vtkXMLPolyDataWriter>::New();
 //        writer->SetInputData(vtk_output_polydata);
-//        writer->SetFileName("test-2output.vtp");
+//        writer->SetFileName("mfx_vtk_output.vtp");
 //        writer->Write();
-//        printf("Dumped output to test-2output.vtp\n");
+//        printf("Dumped output to mfx_vtk_output.vtp\n");
 //    }
 
     // ------------------------------------------------------------------------
     // Create MFX mesh from vtkPolyData
-    // TODO optimize
+    //
+    // Mapping is as follows:
+    // vertex    <- VTK: vtkVertex, vtkPolyVertex
+    //           -> MFX: point (without vertex or face)
+    // edge      <- VTK: vtkLine, vtkPolyLine
+    //           -> MFX: 2-vertex face(s)
+    // face      <- VTK: vtkTriangle, vtkQuad, vtkPolygon
+    //           -> MFX: n-vertex face
     // ------------------------------------------------------------------------
 
+    auto vtk_output_verts = vtk_output_polydata->GetVerts();
+    auto vtk_output_lines = vtk_output_polydata->GetLines();
     auto vtk_output_polys = vtk_output_polydata->GetPolys();
     auto vtk_output_points = vtk_output_polydata->GetPoints();
 
-    int output_point_count = vtk_output_points->GetNumberOfPoints();
-    int output_vertex_count = vtk_output_polys->GetNumberOfConnectivityIds();
-    int output_face_count = vtk_output_polys->GetNumberOfCells();
+    int output_point_count = 0;
+    int output_vertex_count = 0;
+    int output_face_count = 0;
+
+    if (vtk_output_points != nullptr) {
+        output_point_count += vtk_output_points->GetNumberOfPoints();
+    }
+
+    if (vtk_output_verts != nullptr) {
+        // no contribution to vertex or face count
+    }
+    if (vtk_output_lines != nullptr) {
+        int edge_count = (
+                vtk_output_lines->GetConnectivityArray()->GetNumberOfValues() - 1 // edge count if it was all one line
+                - vtk_output_lines->GetNumberOfCells() + 1 // subtract edges between distinct lines
+                );
+
+        output_vertex_count += 2*edge_count;
+        output_face_count += edge_count;
+    }
+    if (vtk_output_polys != nullptr) {
+        output_vertex_count += vtk_output_polys->GetNumberOfConnectivityIds();
+        output_face_count += vtk_output_polys->GetNumberOfCells();
+    }
 
     // Allocate output MFX mesh
     MfxMesh output = GetInput(kOfxMeshMainOutput).GetMesh();
-    output.Allocate(output_point_count, output_vertex_count, output_face_count);
+    output.Allocate(output_point_count, output_vertex_count, output_face_count); // not a hint, this must be exact
 
     // Get output mesh data
     output.GetPointAttribute(kOfxMeshAttribPointPosition).FetchProperties(pointPos);
     output.GetVertexAttribute(kOfxMeshAttribVertexPoint).FetchProperties(vertPoint);
     output.GetFaceAttribute(kOfxMeshAttribFaceCounts).FetchProperties(faceLen);
+
+    printf("MFX output has %d points, %d vertices, %d faces\n",
+           output_point_count, output_vertex_count, output_face_count);
 
     // copy points
     /*
@@ -186,13 +238,16 @@ OfxStatus VtkEffect::Cook(OfxMeshEffectHandle instance) {
                  3*sizeof(float));
                  */
 
-    for (int point_idx = 0; point_idx < output_point_count; point_idx++) {
-        float *p = ((float *) (&pointPos.data[point_idx * pointPos.stride]));
-        double p_in[3];
-        vtk_output_points->GetPoint(point_idx, p_in);
-        p[0] = (float)p_in[0];
-        p[1] = (float)p_in[1];
-        p[2] = (float)p_in[2];
+    // write points
+    if (vtk_output_points != nullptr) {
+        for (int point_idx = 0; point_idx < output_point_count; point_idx++) {
+            float *p = ((float *) (&pointPos.data[point_idx * pointPos.stride]));
+            double p_in[3];
+            vtk_output_points->GetPoint(point_idx, p_in);
+            p[0] = (float)p_in[0];
+            p[1] = (float)p_in[1];
+            p[2] = (float)p_in[2];
+        }
     }
 
     // copy cells
@@ -217,40 +272,82 @@ OfxStatus VtkEffect::Cook(OfxMeshEffectHandle instance) {
                  sizeof(int));
                  */
 
-    vtk_output_polys->ConvertTo32BitStorage();
     vertex_idx = 0;
-    for (int face_idx = 0; face_idx < output_face_count; face_idx++) {
-        int* mfx_output_count = ((int*)(&faceLen.data[face_idx * faceLen.stride]));
+    int face_idx = 0;
 
-        int vtk_offset_start = vtk_output_polys->GetOffsetsArray32()->GetValue(face_idx);
-        int vtk_offset_end = vtk_output_polys->GetOffsetsArray32()->GetValue(face_idx+1);
-        int vtk_output_count = vtk_offset_end - vtk_offset_start;
+    // write line vertices+faces
+    if (vtk_output_lines != nullptr && vtk_output_lines->GetNumberOfCells() > 0) {
+        printf("Writing lines to MFX output mesh\n");
+        // slow path, accessing buffers directly would be faster
+        auto iter = vtk::TakeSmartPointer(vtk_output_lines->NewIterator());
+        for (iter->GoToFirstCell(); !iter->IsDoneWithTraversal(); iter->GoToNextCell()) {
+            vtkIdType cellSize;
+            const vtkIdType* cellPoints;
+            iter->GetCurrentCell(cellSize, cellPoints);
 
-        *mfx_output_count = vtk_output_count;
+            int* mfx_output_count = (int*)(&faceLen.data[face_idx * faceLen.stride]);
+            int* mfx_output_vertex = (int*)(&vertPoint.data[vertex_idx * vertPoint.stride]);
 
-        // begin cell
-        // XXX check if its polygon or something else
-        for (int i = 0; i < vtk_output_count; i++, vertex_idx++) {
-            int* mfx_output_vertex = ((int*)(&vertPoint.data[vertex_idx * vertPoint.stride]));
+            for (int i = 0; i < cellSize - 1; i++) {
+                *mfx_output_count = 2;
+                face_idx++;
 
-            int vtk_output_vertex = vtk_output_polys->GetConnectivityArray32()->GetValue(vtk_offset_start + i);
-            *mfx_output_vertex = vtk_output_vertex;
+                mfx_output_vertex[0] = cellPoints[i];
+                mfx_output_vertex[1] = cellPoints[i+1];
+                vertex_idx += 2;
+            }
         }
-        // end cell
+    }
+
+    // write poly vertices+faces
+    if (vtk_output_polys != nullptr && vtk_output_polys->GetNumberOfCells() > 0) {
+        printf("Writing polys to MFX output mesh\n");
+        vtk_output_polys->ConvertTo32BitStorage();
+        int poly_face_idx = 0;
+        for (int i = 0; i < output_face_count; i++, face_idx++, poly_face_idx++) {
+            int* mfx_output_count = ((int*)(&faceLen.data[face_idx * faceLen.stride]));
+
+            int vtk_offset_start = vtk_output_polys->GetOffsetsArray32()->GetValue(poly_face_idx);
+            int vtk_offset_end = vtk_output_polys->GetOffsetsArray32()->GetValue(poly_face_idx+1);
+            int vtk_output_count = vtk_offset_end - vtk_offset_start;
+
+            *mfx_output_count = vtk_output_count;
+
+            // begin cell
+            // XXX check if its polygon or something else
+            for (int j = 0; j < vtk_output_count; j++, vertex_idx++) {
+                int* mfx_output_vertex = ((int*)(&vertPoint.data[vertex_idx * vertPoint.stride]));
+
+                int vtk_output_vertex = vtk_output_polys->GetConnectivityArray32()->GetValue(vtk_offset_start + j);
+                *mfx_output_vertex = vtk_output_vertex;
+            }
+            // end cell
+        }
     }
 
     // TODO handle attributes
+
+    auto t_cook_after_vtk_epilogue = std::chrono::system_clock::now();
 
     // Release meshes
     input.Release();
     output.Release();
 
-    auto t_cook_end = std::chrono::system_clock::now();
+    auto t_cook_after_mfx_epilogue = std::chrono::system_clock::now();
 
-    auto t_brutto = std::chrono::duration_cast<std::chrono::milliseconds>(t_cook_end - t_cook_start).count();
-    auto t_netto = std::chrono::duration_cast<std::chrono::milliseconds>(t_cook_inner_end - t_cook_inner_start).count();
+    auto dt = [](std::chrono::system_clock::time_point t1, std::chrono::system_clock::time_point t2) -> int {
+        return std::chrono::duration_cast<std::chrono::milliseconds>(t2 - t1).count();
+    };
 
-    printf("\n\tVtkEffect cooked in %d ms (+ %d ms in wrapper)\n\n", (int)t_netto, (int)(t_brutto - t_netto));
+    int t_brutto = dt(t_cook_start, t_cook_after_mfx_epilogue);
+    int t_netto = dt(t_cook_after_vtk_prologue, t_cook_after_vtk_cook);
+    int t_mfx_prologue = dt(t_cook_start, t_cook_after_mfx_prologue);
+    int t_mfx_epilogue = dt(t_cook_after_vtk_epilogue, t_cook_after_mfx_epilogue);
+    int t_vtk_prologue = dt(t_cook_after_mfx_prologue, t_cook_after_vtk_prologue);
+    int t_vtk_epilogue = dt(t_cook_after_vtk_cook, t_cook_after_vtk_epilogue);
 
+    printf("\n\tVtkEffect cooked in %d ms (+ %d ms = prologue/epilogue %d/%d ms VTK, %d/%d ms MFX)\n\n",
+           t_netto, t_brutto-t_netto, t_vtk_prologue, t_vtk_epilogue, t_mfx_prologue, t_mfx_epilogue);
+    printf("==/ VtkEffect::Cook\n");
     return kOfxStatOK;
 }
